7f080fcf491f36d7ef6d1d78a40f08eabdd9a6bc
bmas100 <tosemaskan.co@gmail.com>
2026-07-24 23:10:22 +0330

    Update tracker.js

--- HomeMob/tracker.js
+++ HomeMob/tracker.js
@@ -1,3 +1,96 @@
-console.log("Tracker Loaded");
+const LOG_URL = "https://script.google.com/macros/s/AKfycbwV2BLqtTU789b2I-Na3cGzonALX0AKktQc5pRWcxzkvtBOzLmufd1mCn7YVWKlq4M6/exec";
 
-alert("Tracker Loaded");
+const Tracker = {
+
+    userCode: "",
+    sessionId: "",
+    deviceId: "",
+
+    init() {
+
+        const p = new URLSearchParams(location.search);
+
+        this.userCode = p.get("u") || "";
+
+        this.sessionId = crypto.randomUUID();
+
+        this.deviceId = localStorage.getItem("deviceId");
+
+        if (!this.deviceId) {
+
+            this.deviceId = crypto.randomUUID();
+
+            localStorage.setItem("deviceId", this.deviceId);
+
+        }
+
+    },
+
+    async send(action, target = "", result = "Success") {
+
+        const data = {
+
+            EventID: crypto.randomUUID(),
+
+            UserCode: this.userCode,
+
+            DateTime: new Date().toISOString(),
+
+            SessionID: this.sessionId,
+
+            Page: location.pathname.split("/").pop(),
+
+            Action: action,
+
+            Target: target,
+
+            Result: result,
+
+            IP: "",
+
+            VisitDuration: "",
+
+            DeviceID: this.deviceId,
+
+            Browser: navigator.userAgent,
+
+            OS: navigator.platform,
+
+            Resolution: screen.width + "x" + screen.height,
+
+            Referrer: document.referrer,
+
+            PWA: window.matchMedia("(display-mode: standalone)").matches
+
+        };
+
+        try {
+
+            const r = await fetch(LOG_URL, {
+
+                method: "POST",
+
+                headers: {
+                    "Content-Type": "application/json"
+                },
+
+                body: JSON.stringify(data)
+
+            });
+
+            console.log(await r.text());
+
+        }
+        catch (e) {
+
+            console.error(e);
+
+        }
+
+    }
+
+};
+
+Tracker.init();
+
+Tracker.send("Open");
